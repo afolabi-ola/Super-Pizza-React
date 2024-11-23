333a73657de172af63e27cbbcdd5bb6b6060583b
afolabi-ola <148984714+afolabi-ola@users.noreply.github.com>
2024-11-23 19:16:53 +0100

    Update apiRestaurant.js

--- src/services/apiRestaurant.js
+++ src/services/apiRestaurant.js
@@ -47,7 +47,7 @@ export async function updateOrder(id, updateObj) {
     });
 
     if (!res.ok) throw Error();
-    // We don't need the data, so we don't return anything
+    // We don't need the data, so we don't return anythin
   } catch (err) {
     throw Error('Failed updating your order');
   }
